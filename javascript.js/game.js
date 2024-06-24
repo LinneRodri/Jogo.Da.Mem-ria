5cd1fe2d9db2be2e0ee01333fce71a9ce4606c42
LinneRodri <alinneoliveira095@gmail.com>
2024-06-24 13:58:40 -0300

    Modificações

--- javascript.js/game.js
+++ javascript.js/game.js
@@ -3,16 +3,16 @@ const spanPlayer = document.querySelector('.player');
 const timer = document.querySelector('.timer');
 
 const characters = [
-  'beth',
-  'jerry',
-  'jessica',
-  'morty',
-  'pessoa-passaro',
-  'pickle-rick',
-  'rick',
-  'summer',
-  'meeseeks',
-  'scroopy',
+  'mariovermelho',
+  'dragao',
+  'cabecazul',
+  'cabecavermelha',
+  'luiginovo',
+  'macacomario',
+  'waluigi',
+  'yoshi',
+  'princesanovo',
+  'wario',
 ];
 
 const createElement = (tag, className) => {
